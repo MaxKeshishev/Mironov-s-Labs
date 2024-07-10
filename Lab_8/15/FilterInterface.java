1f8d82dfe7108ac4fc93b98e2e1fa477b75ff2f3
MaxKeshishev <maxim.keshishev@gmail.com>
2024-07-11 00:00:19 +0200

    Delete Lab_8/15 directory

--- Lab_8/15/FilterInterface.java
+++
@@ -1,4 +0,0 @@
-// обобщённый функциональный интерфейс для лямбда-выражений фильтрации
-public interface FilterInterface <T> {
-    boolean func(T filter, Product P);
-}
--- Lab_8/15/SortInterface.java
+++
@@ -1,4 +0,0 @@
-// функциональный интерфейс для ссылки на методы сортировки
-public interface SortInterface {
-    boolean func(Product First, Product Second); // передаём массив продуктов и индекс последнего элемента в нём
-}
